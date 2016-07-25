917f8cac816b801ec841a7a5fc1e15afbe68a0af
future-challenger <sunshine_anycall@hotmail.com>
2016-07-25 23:07:43 +0800

    Initial Commit

---
+++ AFDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AFDemo
+//
+//  Created by Bruce Lee on 25/7/2016.
+//  Copyright © 2016 Dynamic Cell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
